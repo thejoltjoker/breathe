6ceed71a7b86914f541c777c6d5cfd1e9ea7d4d7
Johannes Andersson <hello@thejoltjoker.com>
2025-01-04 01:43:17 +0100

    feat: remove clock

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import "@fontsource-variable/inter";
 import "./style.css";
 import "./theme.ts";
-import "./clock.ts";
+
 import { initPWA } from "./pwa.ts";
 
 const app = document.querySelector<HTMLDivElement>("#app")!;
