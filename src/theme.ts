36d6f3e17d711c5592258af163665a1225853b0d
Johannes Andersson <hello@thejoltjoker.com>
2025-01-04 01:26:59 +0100

    feat: update for iPhone

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,6 @@
 const app = document.querySelector<HTMLDivElement>("#app")!;
-const theme = localStorage.getItem("theme") ?? null;
-if (localStorage.getItem("theme") == "dark-theme")
-  document.body.classList.add("dark-theme");
+const useDarkTheme = localStorage.getItem("theme") ?? null;
+if (useDarkTheme == "dark-theme") document.body.classList.add("dark-theme");
 
 app.addEventListener("click", function () {
   document.body.classList.toggle("dark-theme");
@@ -9,4 +8,15 @@ app.addEventListener("click", function () {
     "theme",
     document.body.classList.contains("dark-theme") ? "dark-theme" : ""
   );
+
+  const isDark = document.body.classList.contains("dark-theme");
+  document.documentElement.style.setProperty(
+    "--status-bar-background",
+    isDark ? "var(--dark)" : "var(--light)"
+  );
 });
+
+document.documentElement.style.setProperty(
+  "--status-bar-background",
+  useDarkTheme === "dark-theme" ? "var(--dark)" : "var(--light)"
+);
