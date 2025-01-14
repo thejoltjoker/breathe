4604bddb5869a9baa41e5acde99cfa19f9d91920
Johannes Andersson <hello@thejoltjoker.com>
2025-01-14 19:34:16 +0100

    feat: add instructions

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,39 @@
-import "@fontsource-variable/inter";
+import "@fontsource-variable/onest";
 import "./style.css";
 import "./theme.ts";
-
 import { initPWA } from "./pwa.ts";
 
+const sequence = ["Breathe in", "Hold", "Breathe out", "Hold"];
+const FADE_DURATION = 1000;
+const HOLD_DURATION = 3000;
 const app = document.querySelector<HTMLDivElement>("#app")!;
+const textContainer =
+  document.querySelector<HTMLDivElement>(".text-container")!;
 
 initPWA(app);
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const animateSequence = async () => {
+  let currentIndex = 0;
+
+  while (true) {
+    if (currentIndex > sequence.length) break;
+
+    textContainer.style.transition = `opacity ${FADE_DURATION}ms`;
+    textContainer.style.opacity = "0";
+
+    if (currentIndex != 0) {
+      await sleep(FADE_DURATION);
+    }
+
+    textContainer.textContent = sequence[currentIndex];
+    textContainer.style.opacity = "1";
+
+    await sleep(HOLD_DURATION);
+
+    currentIndex = currentIndex + 1;
+  }
+};
+
+animateSequence();
